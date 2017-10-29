cfc7f66637c98c5f5538806e0062d0944f88e6e0
Steven Ciasullo <sjciasullo@gmail.com>
2017-10-29 19:37:58 -0400

    user model

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const db = require('../db/config');
+
+const User = {};
+
+User.findByUserName = (username) => {
+  return db.one(
+    `
+    SELECT * FROM users
+    WHERE username = $1
+    `, [username]
+  );
+};
+
+User.create = (user) => {
+  return db.one(
+    `
+    INSERT INTO users
+    (username, password_digest, email, dj_name)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *
+    `, [user.username, user.password_digest, user.email, user.dj_name]
+  );
+};
+
+module.exports = User;
