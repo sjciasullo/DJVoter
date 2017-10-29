3d2e88b91ca21e3530f1b3f15f660610687af66a
Steven Ciasullo <sjciasullo@gmail.com>
2017-10-29 19:27:05 -0400

    config db

---
+++ db/config.js
@@ -0,0 +1,22 @@
+const options = {
+  query: (e) => {
+    console.log(e.query);
+  },
+};
+
+const pgp = require('pg-promise')(options):
+
+function setDb() {
+  if (process.env.NODE_ENV = 'development' || !process.env.NODE_ENV) {
+    return pgp({
+      database: 'dj_voter',
+      port: 5432,
+      host: 'localhost',
+    });
+  } else if (process.env.NODE_ENV === 'production') {
+    return pgp(process.env.DATABASE_URL);
+  }
+}
+
+const db = setDb();
+module.exports = db;
