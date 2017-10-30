ffcd6b883e398f2df1a935ecd09ed6ee20d698df
Steven Ciasullo <sjciasullo@gmail.com>
2017-10-29 21:02:01 -0400

    change migrations email to jamendo name

--- db/migrations/migration-1029171830.sql
+++ db/migrations/migration-1029171830.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS users (
   id SERIAL PRIMARY KEY,
   username VARCHAR(255) UNIQUE NOT NULL,
   password_digest TEXT NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL,
+  jamendo_name VARCHAR(255) UNIQUE NOT NULL,
   dj_name VARCHAR(255)
 );
 
